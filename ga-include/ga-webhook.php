ec6c9cabc723589e4d32541c2a79f053b567686f
Xiangyu Bu <xybu92@live.com>
2015-01-07 00:43:56 -0800

    Fix dumb syntax error

--- ga-include/ga-webhook.php
+++ ga-include/ga-webhook.php
@@ -62,7 +62,7 @@ class GitLab_WebHook extends Base {
 			];
 			$file_name = $this->data['project_id'] . '_' . $delegate_key . '.json';
 			
-			if (!is_dir(getcwd() . '/pushes')) mkdir(getcwd() . '/pushes', 0770)
+			if (!is_dir(getcwd() . '/pushes')) mkdir(getcwd() . '/pushes', 0770);
 			
 			file_put_contents(getcwd() . '/pushes/' . $file_name, json_encode($file_data));
 			
